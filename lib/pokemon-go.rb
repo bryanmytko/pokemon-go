9c17eab6128f4a6038a7342c4f11a8c98591c560
Bryan Mytko <bryanmytko@gmail.com>
2016-07-21 17:13:17 -0400

    Initial commit

---
+++ lib/pokemon-go.rb
@@ -0,0 +1,19 @@
+require "./pokemon.pb.rb"
+
+API_URL = "https://pgorelease.nianticlabs.com/plfe/rpc"
+LOGIN_URL = 'https://sso.pokemon.com/sso/login?service=https%3A%2F%2Fsso.pokemon.com%2Fsso%2Foauth2.0%2FcallbackAuthorize'
+LOGIN_OAUTH = 'https://sso.pokemon.com/sso/oauth2.0/accessToken'
+PTC_CLIENT_SECRET = 'w8ScCUXJQc6kXKw8FiOhd8Fixzht18Dq3PEVkUCP5ZPxtgyWsbTvWHFLm2wNY0JR'
+
+CONFIG = "config.json"
+
+# {
+#   "auth_service": "google",
+#   "username": "example@gmail.com",
+#   "password": "password11!!",
+#   "location": "New York",
+#   "debug": true,
+#   "client_secret": "000000000000000000"
+# }
+#
+
