b9bb43281b51a269ae1fcd08560a41fd11dd15ff
Bryan Mytko <bryanmytko@gmail.com>
2016-07-21 18:29:34 -0400

    Working on Google OAuth

--- lib/pokemon-go.rb
+++ lib/pokemon-go.rb
@@ -1,19 +1,30 @@
-require "./pokemon.pb.rb"
+require "./lib/pokemon.pb.rb"
+require 'google/api_client/client_secrets'
+require "google/apis/games_management_v1management"
+
+require 'google/api_client'
+require 'google/api_client/client_secrets'
+require 'google/api_client/auth/file_storage'
+require 'google/api_client/auth/installed_app'
+
+# @TODO dev
+require 'byebug'
 
 API_URL = "https://pgorelease.nianticlabs.com/plfe/rpc"
 LOGIN_URL = 'https://sso.pokemon.com/sso/login?service=https%3A%2F%2Fsso.pokemon.com%2Fsso%2Foauth2.0%2FcallbackAuthorize'
 LOGIN_OAUTH = 'https://sso.pokemon.com/sso/oauth2.0/accessToken'
 PTC_CLIENT_SECRET = 'w8ScCUXJQc6kXKw8FiOhd8Fixzht18Dq3PEVkUCP5ZPxtgyWsbTvWHFLm2wNY0JR'
 
-CONFIG = "config.json"
+ANDROID_ID = '9774d56d682e549c'
+SERVICE= 'audience:server:client_id:848232511240-7so421jotr2609rmqakceuu1luuq0ptb.apps.googleusercontent.com'
+APP = 'com.nianticlabs.pokemongo'
+CLIENT_SIG = '321187995bc7cdc2b5fc91b11a96e2baa8602c62'
 
-# {
-#   "auth_service": "google",
-#   "username": "example@gmail.com",
-#   "password": "password11!!",
-#   "location": "New York",
-#   "debug": true,
-#   "client_secret": "000000000000000000"
-# }
-#
+def google_login
+  # CLIENT_SECRETS = Google::APIClient::ClientSecrets.load
+  # authorization = CLIENT_SECRETS.to_authorization
 
+  # Google::Apis::GamesManagementV1management.new
+end
+
+google_login
