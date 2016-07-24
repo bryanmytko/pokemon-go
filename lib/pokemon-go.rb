b2fb4ae10cd6c15ca476a5f78dd28dbbe4b2119d
Bryan Mytko <bryanmytko@gmail.com>
2016-07-24 14:55:13 -0400

    Finish google_login

--- lib/pokemon-go.rb
+++ lib/pokemon-go.rb
@@ -1,9 +1,9 @@
 require "./lib/pokemon.pb.rb"
-require 'google/api_client/client_secrets'
-require "google/apis/games_management_v1management"
+require "gpsoauth"
 
 # @TODO dev
 require 'byebug'
+require 'io/console'
 
 API_URL = "https://pgorelease.nianticlabs.com/plfe/rpc"
 LOGIN_URL = 'https://sso.pokemon.com/sso/login?service=https%3A%2F%2Fsso.pokemon.com%2Fsso%2Foauth2.0%2FcallbackAuthorize'
@@ -15,7 +15,18 @@ SERVICE= 'audience:server:client_id:848232511240-7so421jotr2609rmqakceuu1luuq0pt
 APP = 'com.nianticlabs.pokemongo'
 CLIENT_SIG = '321187995bc7cdc2b5fc91b11a96e2baa8602c62'
 
-def google_login
+def google_login(email, password)
+  g = Gpsoauth::Connection.new(ANDROID_ID)
+  response = g.master_login(email, password)
+  oauth = g.oauth(email, response["Token"], SERVICE, APP, CLIENT_SIG)
+
+  oauth["Auth"]
 end
 
-google_login
+print "Email: "
+email = gets.chomp
+
+print "Password: "
+password = STDIN.noecho(&:gets)
+
+puts "\nYour OAuth token for using Pokemon Go is: #{google_login(email, password)}"
